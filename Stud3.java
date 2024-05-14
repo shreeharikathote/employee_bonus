d60bcaddef81432d861d0f7832baf01630315252
01fe22bca086 <01fe22bca086@kletech.ac.in>
2024-05-14 18:41:09 +0530

    first commit

---
+++ Stud3.java
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+class Stud3:
+    def _init_(self, name, dob):
+        self.name = name
+        self.dob = datetime.strptime(dob, "%d-%m-%Y") if "-" in dob else datetime.strptime(dob, "%Y-%m-%d")
+
+    def display_info(self):
+        age = (datetime.now() - self.dob).days // 365
+        print(f"Name: {self.name}")
+        print(f"Age: {age}")
